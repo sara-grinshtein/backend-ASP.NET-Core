aff5a4a3c6e4ed79bd3f7a59877e22955c16333a
sara-grinshtein <sry3038@gmail.com>
2025-07-06 23:04:40 +0300

    fix areas of knowleges

--- Mock/DataBaseFactory.cs
+++ Mock/DataBaseFactory.cs
@@ -1,22 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Design;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore.Design;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace Mock
+//{
+//    // DataBaseFactory.cs
+//    public class DataBaseFactory : IDesignTimeDbContextFactory<DataBase>
+//    {
+//        public DataBase CreateDbContext(string[] args)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<DataBase>();
+//            optionsBuilder.UseSqlServer("Server=localhost;Database=project_yedidim1;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;");
+//            return new DataBase(optionsBuilder.Options);
+//        }
+//    }
+
+//}
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Mock
 {
-    // DataBaseFactory.cs
     public class DataBaseFactory : IDesignTimeDbContextFactory<DataBase>
     {
         public DataBase CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<DataBase>();
-            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=project_yedidim1;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;");
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=project_yedidim1;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False;"
+            );
             return new DataBase(optionsBuilder.Options);
         }
     }
-
 }
+
