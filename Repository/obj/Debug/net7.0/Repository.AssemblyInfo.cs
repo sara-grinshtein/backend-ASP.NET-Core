93f6e1d545cbb1c44d7210c6fd36432249eee120
sara-grinshtein <sry3038@gmail.com>
2025-10-25 20:16:29 +0300

    update changes to DB 3

--- Repository/obj/Debug/net7.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net7.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc61e1ed550ff92970c84728b325a988b31987f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9df6a8d1337f17893ff8dfaef179157610e53a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
