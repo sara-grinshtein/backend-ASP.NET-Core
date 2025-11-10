ad5f2f0868a7b6b601d87921370d3773af085731
sara-grinshtein <sry3038@gmail.com>
2025-11-10 11:47:59 +0200

    update hrlpeddto

--- Common/Dto/HelpedDto.cs
+++ Common/Dto/HelpedDto.cs
@@ -11,11 +11,11 @@ namespace Common.Dto
     {
         public int helped_id { get; set; }
         public string password { get; set; }
-        public string helped_first_name { get; set; } //שם פרטי
-        public string? helped_last_name { get; set; } //משפחה 
-        public string tel { get; set; }//טל   
+        public string helped_first_name { get; set; } 
+        public string? helped_last_name { get; set; } 
+        public string tel { get; set; }  
         public string email { get; set; }
-        public string location { get; set; }
+     //   public string location { get; set; }
         public bool IsDeleted { get; set; }
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
